29b5e204d2da31c9e8e267279d1dfcbebd641a1e
jason-weirather <jason-weirather@users.noreply.github.com>
2017-07-30 15:35:54 -0400

    updating to look on mobile

--- _build/pelicanconf.py
+++ _build/pelicanconf.py
@@ -8,7 +8,7 @@ SITENAME = 'fasta.io'
 SITESUBTITLE1 = 'a bioinformatics engineer going on adventures;'
 SITESUBTITLE2 = 'lost in a random forest of trees';
 SITEEMOJIS = '🌳🌲🌳🤓🗺🌴🌳🌲'
-SITEURL = 'http://fasta.io'
+SITEURL = 'http://localhost:8000'
 THEME = "../fasta-theme/"
 
 def get_uuid4_string(not_used):
